49c9acabeb226d08b564d9f6595b9d4686588cd9
Daria Pakshina <daradasa7@yandex.ru>
2025-02-10 18:36:23 +0300

    refactor: add routerlink to home and signals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { tuiAppearance, TuiIcon, TuiSurface, TuiTitle } from '@taiga-ui/core';
 import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-home',
-  imports: [TuiIcon, TuiCardLarge, TuiHeader, TuiSurface, TuiTitle],
+  imports: [TuiIcon, TuiCardLarge, TuiHeader, TuiSurface, TuiTitle, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TuiIcon } from '@taiga-ui/core';
 import { TuiNavigation } from '@taiga-ui/layout';
 import { TuiBadge } from '@taiga-ui/kit';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [TuiNavigation, TuiIcon, TuiBadge],
+  imports: [TuiNavigation, TuiIcon, TuiBadge, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
