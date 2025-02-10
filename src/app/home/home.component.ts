21c927b806ee1c04d23565e92c47b5c7630548d1
Daria Pakshina <daradasa7@yandex.ru>
2025-02-10 18:19:15 +0300

    style: home page, navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { tuiAppearance, TuiIcon, TuiSurface, TuiTitle } from '@taiga-ui/core';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TuiIcon, TuiCardLarge, TuiHeader, TuiSurface, TuiTitle],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
