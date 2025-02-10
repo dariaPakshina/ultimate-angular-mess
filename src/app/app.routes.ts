338cfd4c37e9d59bfeaa6fe28a3635fa0487ac91
Daria Pakshina <daradasa7@yandex.ru>
2025-02-10 18:33:34 +0300

    refactor: add home and signals routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'signals',
+    title: 'UAM: Signals',
+    loadComponent: () =>
+      import('./signals/signals.component').then((mod) => mod.SignalsComponent),
+  },
+  { path: '', component: HomeComponent },
+];
