e1783a71769f80bce8ef4d3b50fe4cf4ba1be058
Daria Pakshina <daradasa7@yandex.ru>
2025-02-11 21:28:30 +0300

    feat: carousel with create/set signal

---
+++ src/app/code.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[code]',
+  standalone: true,
+})
+export class CodeDirective {
+  constructor(el: ElementRef) {
+    el.nativeElement.style.padding = '0.75rem';
+    el.nativeElement.style.fontFamily = 'monospace';
+    el.nativeElement.style.color = '#66a80f';
+    el.nativeElement.style.border = '1px solid var(--tui-border-normal)';
+    el.nativeElement.style.borderRadius = '1rem';
+  }
+}
--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CodeDirective } from '../code.directive';
+import { TuiButton } from '@taiga-ui/core';
+import { TuiCarousel } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-signals',
-  imports: [],
+  imports: [CodeDirective, TuiButton, TuiCarousel],
   templateUrl: './signals.component.html',
-  styleUrl: './signals.component.scss'
+  styleUrl: './signals.component.scss',
 })
 export class SignalsComponent {
+  count = signal(0);
 
+  onChangeSignal(count: number) {
+    this.count.set(count);
+  }
 }
