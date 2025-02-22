af3c9652092ca53ca950f6de2e3b53e19b3de1c9
Daria Pakshina <daradasa7@yandex.ru>
2025-02-22 11:35:45 +0300

    feat: carousel with computed signal

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,18 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CodeDirective } from '../code.directive';
 import { TuiButton } from '@taiga-ui/core';
 import { TuiCarousel } from '@taiga-ui/kit';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-signals',
-  imports: [CodeDirective, TuiButton, TuiCarousel],
+  imports: [CodeDirective, TuiButton, TuiCarousel, NgIf],
   templateUrl: './signals.component.html',
   styleUrl: './signals.component.scss',
 })
 export class SignalsComponent {
-  count = signal(0);
+  count = signal(1);
+  doubleCount = computed(() => this.count() * 2);
+
+  switched = false;
 
   onChangeSignal(count: number) {
     this.count.set(count);
+  }
+
+  onSwitch() {
+    this.switched = !this.switched;
   }
 }
