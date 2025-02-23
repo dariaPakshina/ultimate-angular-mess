9c2c7c1529293bd9156161ac8af2af9b4be46925
Daria Pakshina <daradasa7@yandex.ru>
2025-02-23 14:48:23 +0300

    style: add input carousel to components

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CodeDirective } from '../code.directive';
+import { TuiButton, TuiIcon, TuiLink } from '@taiga-ui/core';
+import { TuiCarousel } from '@taiga-ui/kit';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-components',
-  imports: [],
+  imports: [CodeDirective, TuiButton, TuiCarousel],
   templateUrl: './components.component.html',
-  styleUrl: './components.component.scss'
+  styleUrl: './components.component.scss',
 })
-export class ComponentsComponent {
-
-}
+export class ComponentsComponent {}
